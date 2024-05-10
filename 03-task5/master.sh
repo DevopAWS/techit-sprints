1f24bbeb3bf5cff718929a80a4d633c4d4cd63c9
gsurash7 <gsurash7@gmail.com>
2024-05-10 14:46:18 +0530

    task

--- 03-task5/master.sh
+++ 03-task5/master.sh
@@ -13,3 +13,19 @@ yum install jenkins -y
 systemctl start jenkins
 systemctl status jenkins
 systemctl enable jenkins
+
+
+# install kubectl 
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+chmod +x kubectl
+
+mv kubectl /usr/local/bin/kubectl
+
+VALIDATE $? "Kubectl installation"
+
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+
+sudo mv /tmp/eksctl /usr/local/bin
